f7b91f6a26014c086a85f84c0bd0f29f45337ac7
Roman Vinnick <romanvinnick@gmail.com>
2023-07-22 12:06:58 +0200

    adding aliases

---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,25 @@
+import SidebarMenu from '@/components/SidebarMenu/SidebarMenu'
+import MainArea from '@/components/MainArea/MainArea'
+import Split from 'react-split'
+
+const MainLayout: React.FC = () => {
+  return (
+    <>
+      <Split
+        className="app"
+        sizes={[30, 70]}
+        minSize={100}
+        expandToMin={false}
+        gutterSize={10}
+        gutterAlign="center"
+        snapOffset={30}
+        dragInterval={1}
+        direction="horizontal"
+        cursor="col-resize"
+      >
+        <SidebarMenu />
+        <MainArea />
+      </Split>
+    </>
+  )
+}
